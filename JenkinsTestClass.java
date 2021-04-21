44cb5871d8133311f9501818d16b2c8aed4d6d29
Maddy <maddy@gmail.com>
2021-04-21 20:34:11 +0800

    Iterations changed to 12

--- JenkinsTestClass.java
+++ JenkinsTestClass.java
@@ -4,7 +4,7 @@ public class JenkinsTestClass {
 
 	public static void main(String[] args) {
 
-		for(int i = 0; i < 15; i++) {
+		for(int i = 0; i < 13; i++) {
 			System.out.println("Hello World " + i);
 		}
 	}
